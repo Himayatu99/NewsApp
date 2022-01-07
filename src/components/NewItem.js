698d09b604c9ec08c91f0b22d2c2512ebdd0db2b
khan <himayatu99@gmail.com>
2022-01-07 15:37:36 +0500

    Add New.js , NewItem.js

---
+++ src/components/NewItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class NewItem extends Component {
+    render() {
+        return (
+            <div>
+
+                This is a newitem component
+            </div>
+        )
+    }
+}
---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NewItem from './NewItem'
+
+export default class News extends Component {
+    render() {
+        return (
+            <div>
+                this Is new component
+                <NewItem />
+            </div>
+        )
+    }
+}
